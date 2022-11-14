50b7fe95386942d5f6ef81c89add515f6a891f8e
Clara Serruau <56151221+ClaraSerr@users.noreply.github.com>
2022-11-14 09:46:58 -0500

    change aws cli key

--- script_setup.sh
+++ script_setup.sh
@@ -14,10 +14,7 @@ mv ~/Downloads/labsuser.pem ~/Documents/LOG8415/LOG8415_Final_Project/
 chmod 400 labsuser.pem
 
 # Edit AWS CLI
-echo "[default]
-aws_access_key_id=ASIAXRQOSDSKNUAP5THD
-aws_secret_access_key=8dwsqDyR682Qi/Rf+dGnSMZv9ODkG4tZmHokCN/9
-aws_session_token=FwoGZXIvYXdzEOH//////////wEaDH+cGDiI69t2Ar7jRSLCAVEFxvB1bcHt3xkTDrnWDt4K/+4N2MzSIGUiqYUTLRGalvYaXj8Wj0tNwfUXX7tOLbrLic+EB9uhUZ4c/UlE7O520mWgXPoFT2+McjHvuHCgxJGcZP5Udy7IYjrBX+xoN8ZbemqctAaun+3yr0mQhVEeuxgFpvZdnGoQO9ExBPvdaOKFi82or6CRDs2gjRXEhg3ki4215p8ekyeudgTz3FbM729ieLQ+DVmp76UZ4HKatXQEqoYbBYmU/LwT3hfSalmPKL+FxpsGMi3jzz2nPApm3NsYDJoRw6WYaraquy+vNQyVukK2CZ6SFUPpCW0ECydcphNtV1s=" > ~/.aws/credentials
+echo "<AWS CLI>" > ~/.aws/credentials
 
 # Start the execution
 python3 launching.py
