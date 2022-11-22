fc238ef9a3639a61cca94c8389f9765b90b5c488
ClaraSerr <clara.serruau4@etu.univ-lorraine.fr>
2022-11-22 11:48:40 -0500

    Both setups completed, sakila mounted, comments added

--- launching.py
+++ launching.py
@@ -39,8 +39,7 @@ def create_security_group(Vpcid, ports):
                                              VpcId=Vpcid,
                                              )
         security_group_id = security_group.group_id
-        # TODO TO DO add inbound rule ALL ICMP - IPv4 
-        for port in ports: # In our use case, ports = [22, 80, 443]
+        for port in ports: # In our use case, ports = [22, 80, 443, 1186, 3306]
             security_group.authorize_ingress(
                 DryRun=False,
                 IpPermissions=[
@@ -51,7 +50,7 @@ def create_security_group(Vpcid, ports):
                         'IpRanges': [
                             {
                                 'CidrIp': '0.0.0.0/0',
-                                'Description': "Flask_authorize"
+                                'Description': "MySQL"
                             },
                         ]
                     }
@@ -67,7 +66,40 @@ def create_security_group(Vpcid, ports):
                         'IpRanges': [
                             {
                                 'CidrIp': '0.0.0.0/0',
-                                'Description': "Flask_authorize"
+                                'Description': "MySQL"
+                            },
+                        ]
+                    }
+                ]
+            )
+        # add ICMP-IPv4 inbound and outbound rule for all ports
+        security_group.authorize_ingress(
+                DryRun=False,
+                IpPermissions=[
+                    {
+                        'FromPort': -1,
+                        'ToPort': -1,
+                        'IpProtocol': 'ICMP',
+                        'IpRanges': [
+                            {
+                                'CidrIp': '0.0.0.0/0',
+                                'Description': "MySQL"
+                            },
+                        ]
+                    }
+                ]
+            )
+        ec2_CLIENT.authorize_security_group_egress(
+                GroupId=security_group_id,
+                IpPermissions=[
+                    {
+                        'FromPort': -1,
+                        'ToPort': -1,
+                        'IpProtocol': 'ICMP',
+                        'IpRanges': [
+                            {
+                                'CidrIp': '0.0.0.0/0',
+                                'Description': "MySQL"
                             },
                         ]
                     }
@@ -106,7 +138,7 @@ def create_instance(instance_type,keyname,name,security_id,availability_zone):
         InstanceType=instance_type,
         KeyName=keyname,
         MinCount=1,
-        MaxCount=1,
+        MaxCount=1,,
         # Specify the number of the instances in its Tag Name
         TagSpecifications=[
             {
@@ -138,14 +170,19 @@ def create_commands_stand_alone():
     
     commands = [
         'sudo apt-get update', 
+        'yes | sudo apt-get upgrade',
         'yes | sudo apt-get install mysql-server',
+
         # Download Sakila database
         'wget https://downloads.mysql.com/docs/sakila-db.tar.gz',
 
         # Unpack sakila
         'tar -xf sakila-db.tar.gz',
+
         # Remove compressed folder
         'rm sakila-db.tar.gz',
+
+        # Import and use the sakila database
         'sudo mysql -e "SOURCE sakila-db/sakila-schema.sql;"',
         'sudo mysql -e "SOURCE sakila-db/sakila-data.sql;"',
         'sudo mysql -e "USE sakila;"'
@@ -154,7 +191,8 @@ def create_commands_stand_alone():
 
 def create_commands_cluster():
     """
-    Creates a lists of the commands to run on the cluster instances we connected into via paramiko
+    Creates a lists of the commands to run on all instances of the cluster
+    Follow the tutorial: https://stansantiago.wordpress.com/2012/01/04/installing-mysql-cluster-on-ec2/
 
     Returns
     -------
@@ -164,12 +202,18 @@ def create_commands_cluster():
     commands = [
         'sudo apt-get update',
         'sudo mkdir -p /opt/mysqlcluster/home',
+        # give root permissions to the whole folder
         'sudo chmod -R 777 /opt/mysqlcluster',
-        'cd /opt/mysqlcluster/home',
-        'wget http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.2/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz',
-        'tar xvf mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz',
-        'ln -s mysql-cluster-gpl-7.2.1-linux2.6-x86_64 mysqlc',
-        'rm mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz',
+
+        # download MySQL and put it in the appropriate folder
+        'wget http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.2/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz -P /opt/mysqlcluster/home/',
+        'tar -xvf /opt/mysqlcluster/home/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz -C /opt/mysqlcluster/home/',
+
+        # create Symlink to the decompressed directory
+        'ln -s /opt/mysqlcluster/home/mysql-cluster-gpl-7.2.1-linux2.6-x86_64 /opt/mysqlcluster/home/mysqlc',
+        'rm /opt/mysqlcluster/home/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz',
+
+        # export appropriate paths
         'sudo chmod -R 777 /etc/profile.d',
         'echo "export MYSQLC_HOME=/opt/mysqlcluster/home/mysqlc" > /etc/profile.d/mysqlc.sh',
         'echo "export PATH=$MYSQLC_HOME/bin:$PATH" >> /etc/profile.d/mysqlc.sh',
@@ -179,9 +223,15 @@ def create_commands_cluster():
 
     return commands
 
-def create_commands_cluster_master(DNS_addresses):
+def create_commands_cluster_master_1(DNS_private_addresses):
     """
-    Creates a lists of the commands to run on the master instances we connected into via paramiko
+    Creates a lists of the commands to run only on the master instance we connected into via paramiko
+    Follow the tutorial: https://stansantiago.wordpress.com/2012/01/04/installing-mysql-cluster-on-ec2/
+
+    Parameters
+    ----------
+    DNS_private_addresses : dict(str -> str)
+        Values are the private DNS addresses of each instance. The key is the instance's name.
 
     Returns
     -------
@@ -189,19 +239,20 @@ def create_commands_cluster_master(DNS_addresses):
         list of the commands to run
     """
     commands = [
-        'mkdir -p /opt/mysqlcluster/deploy',
-        'cd /opt/mysqlcluster/deploy',
-        'mkdir conf',
-        'mkdir mysqld_data',
-        'mkdir ndb_data',
-        'cd conf',
+        'mkdir -p /opt/mysqlcluster/deploy/conf',
+        'mkdir -p /opt/mysqlcluster/deploy/mysqld_data',
+        'mkdir -p /opt/mysqlcluster/deploy/ndb_data',
+
+        # create my.cnf file
         '''echo "[mysqld]
 ndbcluster
 datadir=/opt/mysqlcluster/deploy/mysqld_data
 basedir=/opt/mysqlcluster/home/mysqlc
-port=3306" > my.cnf''',
+port=3306" > /opt/mysqlcluster/deploy/conf/my.cnf''',
+
+        # create conf.ini file
         f'''echo "[ndb_mgmd]
-hostname={DNS_addresses["Cluster_Master"]}
+hostname={DNS_private_addresses["Cluster_Master"]}
 datadir=/opt/mysqlcluster/deploy/ndb_data
 nodeid=1
 
@@ -210,31 +261,35 @@ noofreplicas=3
 datadir=/opt/mysqlcluster/deploy/ndb_data
 
 [ndbd]
-hostname={DNS_addresses["Cluster_Slave_1"]}
+hostname={DNS_private_addresses["Cluster_Slave_1"]}
 nodeid=3
 
 [ndbd]
-hostname={DNS_addresses["Cluster_Slave_2"]}
+hostname={DNS_private_addresses["Cluster_Slave_2"]}
 nodeid=4
 
 [ndbd]
-hostname={DNS_addresses["Cluster_Slave_3"]}
+hostname={DNS_private_addresses["Cluster_Slave_3"]}
 nodeid=5
 
 [mysqld]
-nodeid=50" > config.ini''',
-        'cd /opt/mysqlcluster/home/mysqlc',
-        'scripts/mysql_install_db --no-defaults --datadir=/opt/mysqlcluster/deploy/mysqld_data',
-        'sudo /opt/mysqlcluster/home/mysqlc/bin/ndb_mgmd -f /opt/mysqlcluster/deploy/conf/config.ini --initial --configdir=/opt/mysqlcluster/deploy/conf/',
-        
+nodeid=50" > /opt/mysqlcluster/deploy/conf/config.ini''',
 
+        # install and config mysql and node management
+        '/opt/mysqlcluster/home/mysqlc/scripts/mysql_install_db --basedir=/opt/mysqlcluster/home/mysqlc --no-defaults --datadir=/opt/mysqlcluster/deploy/mysqld_data',
+        '/opt/mysqlcluster/home/mysqlc/bin/ndb_mgmd -f /opt/mysqlcluster/deploy/conf/config.ini --initial --configdir=/opt/mysqlcluster/deploy/conf/'
     ]
 
     return commands
 
-def create_commands_cluster_slaves(DNS_addresses):
+def create_commands_cluster_slaves(DNS_private_addresses):
     """
-    Creates a lists of the commands to run on the slave instances we connected into via paramiko
+    Creates a lists of the commands to run on the slave instances we connected into via paramiko to connect them to the management node
+
+    Parameters
+    ----------
+    DNS_private_addresses : dict(str -> str)
+        Values are the private DNS addresses of each instance. The key is the instance's name.
 
     Returns
     -------
@@ -243,22 +298,142 @@ def create_commands_cluster_slaves(DNS_addresses):
     """
     commands = [
         'mkdir -p /opt/mysqlcluster/deploy/ndb_data',
-        f'sudo /opt/mysqlcluster/home/mysqlc/bin/ndbd -c {DNS_addresses["Cluster_Master"]}'
+
+        # link slave nodes to the dns private address of the management node
+        f'/opt/mysqlcluster/home/mysqlc/bin/ndbd -c {DNS_private_addresses["Cluster_Master"]}'
     ]
 
     return commands
 
+def create_commands_cluster_master_2():
+    """
+    Creates a lists of the commands to run only on the master instance to start mysqlc on the management node
+
+    Returns
+    -------
+    list
+        list of the commands to run
+    """
+    commands = [
+        # Start mysqlc management node
+        '/opt/mysqlcluster/home/mysqlc/bin/mysqld --defaults-file=/opt/mysqlcluster/deploy/conf/my.cnf --user=root &'
+    ]
+
+    return commands
+
+def create_commands_cluster_master_3():
+    """
+    Creates a lists of the commands to run only on the master instance to set up the users, password and sakila.
+
+    Returns
+    -------
+    list
+        list of the commands to run
+    """
+    commands = [
+        # create myapp user and set a password
+        '''sudo /opt/mysqlcluster/home/mysqlc/bin/mysql -e "CREATE USER 'myapp'@'%' IDENTIFIED BY 'testpwd';GRANT ALL PRIVILEGES ON * . * TO 'myapp'@'%' IDENTIFIED BY 'MyNewPass' WITH GRANT OPTION MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0 ;"''',
+        '''sudo /opt/mysqlcluster/home/mysqlc/bin/mysql -e "USE mysql; UPDATE user SET plugin='mysql_native_password' WHERE User='root'; FLUSH PRIVILEGES;SET PASSWORD FOR 'root'@'localhost' = PASSWORD('MyNewPass');"''',
+        
+        # Download sakila db
+        'wget https://downloads.mysql.com/docs/sakila-db.tar.gz',
+        'tar -xvf sakila-db.tar.gz',
+        'rm sakila-db.tar.gz',
+
+        # Add sakila to mysql and use it
+        '''/opt/mysqlcluster/home/mysqlc/bin/mysql -h 127.0.0.1 -e "SOURCE sakila-db/sakila-schema.sql;" -u root -pMyNewPass''',
+        '''/opt/mysqlcluster/home/mysqlc/bin/mysql -h 127.0.0.1 -e "SOURCE sakila-db/sakila-data.sql;" -u root -pMyNewPass''',
+        '''/opt/mysqlcluster/home/mysqlc/bin/mysql -h 127.0.0.1 -e "USE sakila;" -u root -pMyNewPass'''
+    ]
+
+    return commands
+
+def ssh_connect_and_execute(paramiko_client, DNS_public_address, paramiko_key, commands):
+    """
+    Connects to the instance via paramiko and executes the given commands
+    Prints out the stdout of the commands
+
+    Parameters
+    ----------
+    paramiko_client: SSHClient
+        The paramiko client we use for connection
+    DNS_public_address : str
+        public DNS address of the instance to connect into
+    paramiko_key: RSAKey
+        the labsuser.pem key used to connect to the AWS session
+    commands: list of str
+        list of the bash commands to run one after another on the instance
+
+    Returns
+    -------
+    None
+    """
+    print("Connecting to ", DNS_public_address)
+    paramiko_client.connect( hostname = DNS_public_address, username = "ubuntu", pkey = paramiko_key )
+    print("Connected")
+    
+    for command in commands:
+        print("Executing {}".format( command ))
+        stdin , stdout, stderr = paramiko_client.exec_command(command)
+
+        while True:
+            print(stdout.readline())
+            if stdout.channel.exit_status_ready():
+                break
+    
+    time.sleep(10)
+
+    return None
+
+def ssh_connect_and_execute_no_print(paramiko_client, DNS_public_address, paramiko_key, commands):
+    """
+    Connects to the instance via paramiko and executes the given commands
+    Doesn't print out anything so it can keep the last process up and running
+
+    Parameters
+    ----------
+    paramiko_client: SSHClient
+        The paramiko client we use for connection
+    DNS_public_address : str
+        public DNS address of the instance to connect into
+    paramiko_key: RSAKey
+        the labsuser.pem key used to connect to the AWS session
+    commands: list of str
+        list of the bash commands to run one after another on the instance
+
+    Returns
+    -------
+    None
+    """
+    print("Connecting to ", DNS_public_address)
+    paramiko_client.connect( hostname = DNS_public_address, username = "ubuntu", pkey = paramiko_key )
+    print("Connected")
+    
+    for command in commands:
+        print("Executing {}".format( command ))
+        stdin , stdout, stderr = paramiko_client.exec_command(command)
+    
+    time.sleep(10)
+
+    return None
+
 def main():
     """
     main script for the final project
+    For every dict, the keys are:
+        "Stand_Alone"
+        "Cluster_Master"
+        "Cluster_Slave_1"
+        "Cluster_Slave_2"
+        "Cluster_Slave_3"
     """
 
     Vpcid = ec2_CLIENT.describe_vpcs().get('Vpcs', [{}])[0].get('VpcId', '')
     
     # Launches custom security group
-    security_group_id = create_security_group(Vpcid, [22, 80, 443])
+    security_group_id = create_security_group(Vpcid, [22, 80, 443, 1186, 3306])
 
-    # Launches all instances
+    # Launches all instances and stores their value (into MySQL) and id (into MySQL_ID)
     MySQL = {}
     MySQL_ID = {}
 
@@ -273,71 +448,57 @@ def main():
         MySQL[f"Cluster_Slave_{i}"]=create_instance("t2.micro","vockey",f"mySQL_Cluster_Slave_{i}",security_group_id,"us-east-1b")
         MySQL_ID[f"Cluster_Slave_{i}"]=MySQL[f"Cluster_Slave_{i}"].instance_id
 
-
-    DNS_addresses={}
+    # Go through each instance to get their addresses
+    DNS_public_addresses={}
+    DNS_private_addresses={}
     IP_addresses={}
 
-    for key in MySQL:
-        MySQL[key].wait_until_running()
+    for instance in MySQL:
+        MySQL[instance].wait_until_running()
+
         # Reload the instance attributes
-        MySQL[key].load()
-        DNS_addresses[key] = MySQL[key].private_dns_name
-        IP_addresses[key] = MySQL[key].public_ip_address
-        print("DNS = ",MySQL[key].private_dns_name)
-        print("IPV4 = ",MySQL[key].public_ip_address)
-        # Enable detailed monitoring
-        MySQL[key].monitor(
-            DryRun=False
-        )
+        MySQL[instance].load()
+        DNS_private_addresses[instance] = MySQL[instance].private_dns_name
+        DNS_public_addresses[instance] = MySQL[instance].public_dns_name
+        IP_addresses[instance] = MySQL[instance].public_ip_address
+        print("DNS public = ",MySQL[instance].public_dns_name)
+        print("DNS private = ",MySQL[instance].private_dns_name)
+        print("IPV4 = ",MySQL[instance].public_ip_address)
     
     # Configure SSH connection to AWS
     k = paramiko.RSAKey.from_private_key_file("labsuser.pem")
     c = paramiko.SSHClient()
     c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    
     # wait to make sure connection will be possible
     time.sleep(10)
 
+    #Stand Alone and sakila
+    ssh_connect_and_execute(c, DNS_public_addresses["Stand_Alone"], k, create_commands_stand_alone())
+
+    #Common Steps on all Nodes
     for key in MySQL:
-        print("Connecting to ", IP_addresses[key])
-        c.connect( hostname = IP_addresses[key], username = "ubuntu", pkey = k )
-        print("Connected")
+        if key!="Stand_Alone":
+            ssh_connect_and_execute(c, DNS_public_addresses[key], k, create_commands_cluster())
+            time.sleep(10)
 
-        commands_sakila = create_commands_sakila()
- 
-        for command in commands_sakila:
-            print("Executing {}".format( command ))
-            stdin , stdout, stderr = c.exec_command(command)
-            print(stdout.read())
-            print(stderr.read())
-        c.close()
-        # The last command to be executed does not send anything to stdout, so we don't read it not to be stuck
-        '''print("Executing {}".format( commands_sakila[-1] ))
-        stdin , stdout, stderr = c.exec_command(commands_sakila[-1])
-        print("Go to http://"+str(IP_addresses_t2[i]))'''
-        with sshtunnel.open_tunnel(
-            (IP_addresses[key], 22),
-            ssh_username="ubuntu",
-            ssh_pkey=k,
-            remote_bind_address=('127.0.0.1', 3306),
-            local_bind_address=('127.0.0.1', 3306)) as tunnel:
-            client = paramiko.SSHClient()
-            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            client.connect('127.0.0.1', 3306)
-            #conn = pymysql.connect(host='127.0.0.1', user="ubuntu", port=tunnel.local_bind_port)
-            query_1 = '''SOURCE sakila-db/sakila-schema.sql;'''
-            query_2 = '''SOURCE sakila-db/sakila-data.sql;'''
-            query_3 = '''USE sakila;'''
-
-            data = pd.read_sql_query(query_1, client)
-            print(data)
-            data = pd.read_sql_query(query_2, client)
-            print(data)
-            data = pd.read_sql_query(query_3, client)
-            print(data)
-            client.close()
-
-    time.sleep(5)
+    #On the Cluster_Master
+    ssh_connect_and_execute(c, DNS_public_addresses["Cluster_Master"], k, create_commands_cluster_master_1(DNS_private_addresses))
     
+    #On the Cluster slaves
+    for i in range(1,4):
+        ssh_connect_and_execute(c, DNS_public_addresses[f"Cluster_Slave_{i}"], k, create_commands_cluster_slaves(DNS_private_addresses))
+
+    #On the Cluster_Master, start the mysqlc management node. We need to wait for the process to iddle before proceeding
+    ssh_connect_and_execute_no_print(c, DNS_public_addresses["Cluster_Master"], k, create_commands_cluster_master_2())
+    time.sleep(180)
+
+    #On the Custer Master, set up user, password and sakila
+    ssh_connect_and_execute(c, DNS_public_addresses["Cluster_Master"], k, create_commands_cluster_master_3())
+    time.sleep(10)
+    
+    print(DNS_public_addresses)
+    c.close()
 
     print('Launching complete')
 
