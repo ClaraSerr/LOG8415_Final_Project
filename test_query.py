81bc76039175f8dd0c6340bfed195d483357c414
ClaraSerr <clara.serruau4@etu.univ-lorraine.fr>
2022-12-23 21:16:59 -0500

    add test for mode 2

--- test_query.py
+++ test_query.py
@@ -10,45 +10,53 @@ ec2_CLIENT = boto3.client('ec2')
 
 instance_proxy = ec2_CLIENT.describe_instances(Filters=[{'Name': 'tag:Name', 'Values': ['proxy']}, {'Name': 'instance-state-name', 'Values': ['running']}])
 instance_master = ec2_CLIENT.describe_instances(Filters=[{'Name': 'tag:Name', 'Values': ['mySQL_Cluster_Master']}, {'Name': 'instance-state-name', 'Values': ['running']}])
+instance_slave_1 = ec2_CLIENT.describe_instances(Filters=[{'Name': 'tag:Name', 'Values': ['mySQL_Cluster_Slave_1']}, {'Name': 'instance-state-name', 'Values': ['running']}])
+instance_slave_2 = ec2_CLIENT.describe_instances(Filters=[{'Name': 'tag:Name', 'Values': ['mySQL_Cluster_Slave_2']}, {'Name': 'instance-state-name', 'Values': ['running']}])
+instance_slave_3 = ec2_CLIENT.describe_instances(Filters=[{'Name': 'tag:Name', 'Values': ['mySQL_Cluster_Slave_3']}, {'Name': 'instance-state-name', 'Values': ['running']}])
 
 # Get the instance ID of the instance whose name is 'proxy'
 Proxy_ip = instance_proxy['Reservations'][0]['Instances'][0]["PublicIpAddress"]
 Master_ip = instance_master['Reservations'][0]['Instances'][0]["PublicIpAddress"]
+Slave_1_ip = instance_slave_1['Reservations'][0]['Instances'][0]["PublicIpAddress"]
+Slave_2_ip = instance_slave_2['Reservations'][0]['Instances'][0]["PublicIpAddress"]
+Slave_3_ip = instance_slave_3['Reservations'][0]['Instances'][0]["PublicIpAddress"]
 
 url = 'http://'+Proxy_ip+'/query'
+headers = {'Content-Type': 'application/json'}
 print(Master_ip)
-data = {
+print(url)
+
+# data_1 = {
+#     'mode': 1,
+#     'host': '127.0.0.1',
+#     'port': 3306,
+# #    'ssh_host': Master_ip,
+#     'ssh_user': 'ubuntu',
+#     'db_user': 'root',
+#     'db_password': 'MyNewPass',
+#     'db_name': 'sakila',
+#     'query': '''SELECT * FROM actor LIMIT 10;'''
+# }
+
+# print(data_1)
+
+# response_1 = requests.post(url, json=data_1, headers=headers)
+
+# print(response_1.text)
+
+data_2 = {
+    'mode': 2,
     'host': '127.0.0.1',
     'port': 3306,
-    'ssh_host': Master_ip,
+    # 'ssh_host': Master_ip,
     'ssh_user': 'ubuntu',
-    'db_host': 'localhost',
     'db_user': 'root',
     'db_password': 'MyNewPass',
     'db_name': 'sakila',
-    'query': '''SELECT * FROM actor LIMIT 10;'''
+    'query': '''SELECT * FROM actor LIMIT 5;'''
 }
+print(data_2)
 
-headers = {'Content-Type': 'application/json'}
-print(url)
-print(data)
+response_2 = requests.post(url, json=data_2, headers=headers)
 
-response = requests.post(url, json=data, headers=headers)
-
-# if response.status_code == 200:
-#     result = response.json()
-print(response.text)
-# else:
-#     print('Request failed')
-# Send a GET request to the app
-
-# response = requests.get('http://3.81.167.97:80/')
-# print(response.text)  # prints 'Hello, World!'
-
-# # Test the /users/<id> route
-# response = requests.get('http://3.81.167.97:80/users/1')
-# print(response)  # prints {'name': 'John', 'age': 30}
-
-# # Test a non-existent user
-# response = requests.get('http://3.81.167.97:80/users/3')
-# print(response)  # prints {'error': 'User not found'}
+print(response_2.text)
